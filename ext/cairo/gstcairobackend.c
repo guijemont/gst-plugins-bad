7982d0435fbcf4ad95cc548848482a9ad4d54726
Guillaume Emont <guijemont@igalia.com>
2013-02-07 20:48:33 +0100

    cairosink: matured definition of backend, added a source and data queue

--- ext/cairo/gstcairobackend.c
+++ ext/cairo/gstcairobackend.c
@@ -5,6 +5,31 @@
 
 static gpointer gst_cairo_backend_thread_init (gpointer data);
 
+static gboolean
+gst_cairo_backend_source_prepare (GSource * source, gint * timeout_);
+static gboolean gst_cairo_backend_source_check (GSource * source);
+static gboolean gst_cairo_backend_source_dispatch (GSource * source,
+    GSourceFunc callback, gpointer user_data);
+
+typedef struct
+{
+  GSource parent;
+  GstCairoBackend *backend;
+} CairoBackendSource;
+
+GSourceFuncs gst_cairo_backend_source_funcs = {
+  gst_cairo_backend_source_prepare,
+  gst_cairo_backend_source_check,
+  gst_cairo_backend_source_dispatch,
+};
+
+static gboolean
+queue_check_full_func (GstDataQueue * queue, guint visible, guint bytes,
+    guint64 time, gpointer checkdata)
+{
+  return visible != 0;
+}
+
 GstCairoBackend *
 gst_cairo_backend_new (GstCairoBackendType backend_type)
 {
@@ -20,8 +45,10 @@ gst_cairo_backend_new (GstCairoBackendType backend_type)
       GST_ERROR ("Unhandled backend type: %d", backend_type);
   }
 
-  if (backend && backend->need_own_thread) {
+  if (backend && backend->need_own_thread && !backend->thread_context) {
     GError *error = NULL;
+
+    backend->thread_context = g_main_context_new ();
     backend->thread =
         g_thread_try_new ("backend thread", gst_cairo_backend_thread_init,
         backend, &error);
@@ -31,6 +58,15 @@ gst_cairo_backend_new (GstCairoBackendType backend_type)
       backend = NULL;
       g_error_free (error);
     }
+  }
+
+  if (backend->thread_context) {
+    backend->queue =
+        gst_data_queue_new (queue_check_full_func, NULL, NULL, NULL);
+    backend->source =
+        g_source_new (&gst_cairo_backend_source_funcs,
+        sizeof (gst_cairo_backend_source_funcs));
+    g_source_attach (backend->source, backend->thread_context);
   }
 
   return backend;
@@ -47,6 +83,11 @@ gst_cairo_backend_destroy (GstCairoBackend * backend)
 #endif
     default:
       GST_ERROR ("Unhandled backend type: %d", backend_type);
+  }
+
+  if (backend->source) {
+    g_source_destroy (backend->source);
+    backend->source = NULL;
   }
 
   if (backend->loop) {
@@ -69,9 +110,8 @@ gst_cairo_backend_thread_init (gpointer data)
 {
   GstCairoBackend *backend = (GstCairoBackend *) data;
 
-  backend->thread_context = g_main_context_new ();
   g_main_context_push_thread_default (backend->thread_context);
-  backned->loop = g_main_loop_new (backend->thread_context, FALSE);
+  backend->loop = g_main_loop_new (backend->thread_context, FALSE);
   g_main_loop_run (backend->loop);
   return NULL;
 }
@@ -88,4 +128,42 @@ gst_cairo_backend_use_main_context (GstCairoBackend * backend,
     return;
   }
   backend->thread_context = g_main_context_ref (context);
+}
+
+static gboolean
+gst_cairo_backend_source_prepare (GSource * source, gint * timeout_)
+{
+  return gst_cairo_backend_source_check (source);
+}
+
+static gboolean
+gst_cairo_backend_source_check (GSource * source)
+{
+  CairoBackendSource *backend_source = (CairoBackendSource *) source;
+
+  return gst_data_queue_is_full (backend_source->backend->queue);
+}
+
+static gboolean
+gst_cairo_backend_source_dispatch (GSource * source,
+    GSourceFunc callback, gpointer user_data)
+{
+  GstDataQueueItem *item = NULL;
+  CairoBackendSource *backend_source = (CairoBackendSource *) source;
+  GstCairoBackend *backend;
+  GstMiniObject *object;
+
+  backend = backend_source->backend;
+
+  if (!gst_data_queue_pop (backend->queue, &item))
+    return FALSE;
+
+  object = item->object;
+
+  if (GST_IS_CAPS (object)) {
+  } else if (GST_IS_QUERY (object)) {
+  }
+
+
+  return TRUE;
 }
--- ext/cairo/gstcairobackend.h
+++ ext/cairo/gstcairobackend.h
@@ -11,23 +11,29 @@ typedef struct _GstCairoBackend GstCairoBackend;
   code). For instance, there might be several back ends for gl surfaces:
   EGL/GLES, GLX, etc...
  */
-struct _GstCairoBackend {
-  cairo_surface_t *     (*create_surface)       (gint width, gint height);
+struct _GstCairoBackend
+{
+  cairo_surface_t *(*create_surface) (gint width, gint height);
 
-  gboolean              need_own_thread;
-  GThread *             thread;
-  GMainContext *        thread_context;
-  GMainLoop *           loop;
+  gboolean need_own_thread;
+  GThread *thread;
+  GMainContext *thread_context;
+  GMainLoop *loop;
+  GSource *source;
+  GstDataQueue *queue;
 };
 
-enum GstCairoBackendType {
-  GST_CAIRO_BACKEND_GLX         = 1,
-  GST_CAIRO_BACKEND_XLIB        = 2,
-};
+typedef enum
+{
+  GST_CAIRO_BACKEND_GLX = 1,
+  GST_CAIRO_BACKEND_XLIB = 2,
+} GstCairoBackendType;
 #define GST_CAIRO_BACKEND_LAST 2
 
-GstCairoBackend * gst_cairo_backend_new (GstCairoBackendType backend_type, GMainContext *context);
-void gst_cairo_backend_use_main_context (GstCairoBackend *backend, GMainContext *context);
-void gst_cairo_backend_create_thread (GstCairoBackend *backend);
+GstCairoBackend *gst_cairo_backend_new (GstCairoBackendType backend_type,
+    GMainContext * context);
+void gst_cairo_backend_use_main_context (GstCairoBackend * backend,
+    GMainContext * context);
+void gst_cairo_backend_create_thread (GstCairoBackend * backend);
 
 #endif /* _GST_CAIRO_BACKEND_H_ */
