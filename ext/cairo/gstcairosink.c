40dcd2ff955abe40d913c9762b0591f7a7336927
Guillaume Emont <guijemont@igalia.com>
2013-04-09 18:12:51 -0500

    cairosink: do unmap in render thread

--- ext/cairo/gstcairosink.c
+++ ext/cairo/gstcairosink.c
@@ -712,6 +712,17 @@ gst_cairo_sink_source_dispatch (GSource * source,
         gst_structure_set_value (query_structure, "mapped-area", &value);
         g_value_unset (&value);
       }
+    } else if (gst_structure_has_name (query_structure,
+            "cairosink-unmap-surface")) {
+      cairo_surface_t *surface;
+      GstCairoBackendSurfaceInfo *surface_info;
+
+      if (!gst_structure_get (query_structure,
+              "surface", G_TYPE_POINTER, &surface,
+              "surface-info", G_TYPE_POINTER, &surface_info, NULL)) {
+        g_assert_not_reached ();
+      }
+      cairosink->backend->surface_unmap (surface, surface_info);
     } else {
       g_assert_not_reached ();
     }
@@ -1054,6 +1065,20 @@ void
 gst_cairo_allocator_mem_unmap (GstMemory * gmem)
 {
   GstCairoMemory *mem = (GstCairoMemory *) gmem;
+  GstCairoAllocator *cairo_allocator = (GstCairoAllocator *) gmem->allocator;
+  GstStructure *query_structure;
+  GstQuery *query;
+  GstFlowReturn ret;
 
-  mem->surface_info->backend->surface_unmap (mem->surface, mem->surface_info);
+  query_structure = gst_structure_new ("cairosink-unmap-surface",
+      "surface", G_TYPE_POINTER, mem->surface,
+      "surface-info", G_TYPE_POINTER, mem->surface_info, NULL);
+  query = gst_query_new_custom (GST_QUERY_CUSTOM, query_structure);
+
+  ret = gst_cairo_sink_sync_render_operation (cairo_allocator->sink,
+      GST_MINI_OBJECT_CAST (query));
+
+  if (ret != GST_FLOW_OK)
+    GST_ERROR_OBJECT (cairo_allocator->sink,
+        "Could not unmap mem %" GST_PTR_FORMAT, gmem);
 }
